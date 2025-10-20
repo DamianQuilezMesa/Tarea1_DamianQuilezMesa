5ea86bcdcd5d67007705326dac5f70c7cf76ce31
Dams <Dams@192.168.1.239>
2025-10-20 14:06:44 +0200

    Quinto commit

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,7 +1,10 @@
 package principal;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.time.LocalDate;
@@ -9,6 +12,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Properties;
 import java.util.Scanner;
 
+import entidades.Credenciales;
 import entidades.Espectaculo;
 import entidades.Perfiles;
 import entidades.Sesion;
@@ -21,68 +25,163 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		menuInvitado(sesion);
-		
+		System.out.println("Bienvenido al Circo!!");
+
+		while (sesion.getPerfil()!=(null)) {
+			selectorMenu(sesion);
+		}
+
 		System.out.println("Fin del programa");
 	}
 
-	public static void menuInvitado(Sesion sesion) {
-		String opcionMenu = "";
-
-		do {
-			System.out.println("Selecciona una opción");
-			System.out.println("1-Ver espectáculos");
-			System.out.println("2-Log in");
-			System.out.println("3-Salir del programa");
-			opcionMenu = sc.nextLine();
-
-			switch (opcionMenu) {
-			case "1":
-				mostrarEspectaculos();
-				break;
-			case "2":
-				logIn(sesion);
-				break;
-			case "3":
-				System.out.println("Saliendo del prorama");
-				break;
-
-			default:
-				System.out.println("Opción no valida, vuelve a intentarlo");
-				break;
-			}
-		} while (!opcionMenu.equals("3"));
-
+	public static void selectorMenu(Sesion sesion) {
+		switch (sesion.getPerfil()) {
+		case INVITADO:
+			menuInvitado(sesion);
+			break;
+		case ADMIN:
+			menuAdmin(sesion);
+			break;
+		case ARTISTA:
+			menuArtista(sesion);
+			break;
+		case COORDINACION:
+			menuCoordinacion(sesion);
+			break;
+		default:
+			System.out.println("Sesion inválida");
+			break;
+		}
 	}
 
-	public static void logIn(Sesion sesion) {
+	public static Sesion logIn(Sesion sesion) {
+		String nomusuario;
+		String contrasenia;
+		boolean credencialesCorrectas = false;
+
+		do {
+			System.out.println(
+					"Introduce tu nombre de usuario (o escribe 'salir' para cancelar):");
+			nomusuario = sc.nextLine().trim();
+			if ("salir".equalsIgnoreCase(nomusuario)) {
+				System.out.println("Login cancelado.");
+				break;
+			}
+
+			System.out.println("Introduce tu contraseña:");
+			contrasenia = sc.nextLine();
+
+			// Asignamos el resultado de las comprobaciones a la variable
+			boolean esAdmin = verificarLoginAdmin(nomusuario, contrasenia,
+					sesion);
+			boolean esUsuario = verificarLoginPerfiles(nomusuario, contrasenia,
+					sesion);
+			credencialesCorrectas = esAdmin || esUsuario;
+
+			if (!credencialesCorrectas) {
+				System.out.println(
+						"Credenciales incorrectas. Vuelve a intentarlo.");
+			}
+		} while (!credencialesCorrectas);
+		return sesion;
+	}
+
+	public static boolean verificarLoginAdmin(String nomusuario,
+			String contrasenia, Sesion sesion) {
+		boolean credencialesCorrectas = false;
+
+		// Cargamos el application.properties para extraer el usuario
+		// y contraseña de admin
 		Properties propiedades = new Properties();
+		// Podria pedir manualmente la ruta si no carga correctamente
 		try {
 			propiedades.load(new FileInputStream(
 					"src/main/resources/application.properties"));
 		} catch (FileNotFoundException e) {
 			System.out.println("No se encuentra el fichero");
+			credencialesCorrectas = false;
 		} catch (IOException e) {
 			System.out.println("El formatin ye incorrectu");
+			credencialesCorrectas = false;
 		}
 
-		String usuarioAdmin = propiedades.getProperty("usuarioadmin");
+		String usuarioAdmin = propiedades.getProperty("usuarioAdmin");
 		String contraseñaAdmin = propiedades.getProperty("passwordAdmin");
 
-		String nomusuario;
-		String contrasenia;
+		if (nomusuario.equals(usuarioAdmin)
+				&& contrasenia.equals(contraseñaAdmin)) {
+			System.out.println("Bienvenido Administrador!");
+			sesion.setPerfil(Perfiles.ADMIN);
+			credencialesCorrectas = true;
+		} else {
+			credencialesCorrectas = false;
+		}
+		return credencialesCorrectas;
+	}
 
-		System.out.println("Introduce tu nombre de usuario");
-		nomusuario = sc.nextLine();
-		System.out.println("Introduce tu contraseña");
-		contrasenia = sc.nextLine();
+	public static boolean verificarLoginPerfiles(String usuario,
+			String contrasena, Sesion sesion) {
+		boolean credencialesCorrectas = false;
 
-		if (nomusuario.equals(usuarioAdmin) && contrasenia.equals(contraseñaAdmin)) {
-            System.out.println("Bienvenido Administrador!");
-            sesion.setPerfil(Perfiles.ADMIN);
-            return;
-        }
+		Credenciales credenciales = new Credenciales();
 
+		File archivo = new File("ficheros/credenciales.txt");
+
+		if (!archivo.exists()) {
+			System.out.println("❌ El archivo credenciales.txt no existe.");
+			credencialesCorrectas = false;
+		}
+
+		try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+			String linea;
+			while ((linea = br.readLine()) != null) {
+				String[] partes = linea.split("\\|");
+				if (partes.length < 7)
+					continue;
+
+				Long id = Long.parseLong(partes[0].trim());
+				String usuarioArchivo = partes[1].trim().toLowerCase();
+				String contrasenaArchivo = partes[2].trim();
+				String perfil = partes[6].trim().toLowerCase();
+
+				if (usuarioArchivo.equals(usuario.toLowerCase().trim())
+						&& contrasenaArchivo.equals(contrasena.trim())) {
+
+					switch (perfil) {
+					case "artista":
+						System.out
+								.println("✅ Usuario autenticado como ARTISTA");
+						credenciales = new Credenciales(id, usuarioArchivo,
+								contrasenaArchivo, Perfiles.ARTISTA);
+						sesion.setCredenciales(credenciales);
+						sesion.setPerfil(Perfiles.ARTISTA);
+						credencialesCorrectas = true;
+						break;
+					case "coordinacion":
+						System.out.println(
+								"✅ Usuario autenticado como COORDINACIÓN");
+						credenciales = new Credenciales(id, usuarioArchivo,
+								contrasenaArchivo, Perfiles.COORDINACION);
+						sesion.setCredenciales(credenciales);
+						sesion.setPerfil(Perfiles.COORDINACION);
+						credencialesCorrectas = true;
+						break;
+					default:
+						System.out
+								.println("⚠️ Perfil no reconocido: " + perfil);
+						credencialesCorrectas = false;
+					}
+
+				} 
+
+			}
+		} catch (IOException e) {
+			System.out.println("❌ Error al leer el archivo de credenciales.");
+			e.printStackTrace();
+
+		}
+
+		return credencialesCorrectas;
 	}
 
 	public static void crearEspectaculo() {
@@ -90,7 +189,7 @@ public class Principal {
 		try {
 			System.out.println("Introduce el identificador (número entero):");
 			Long id = sc.nextLong();// Maximo 25 caracteres
-			sc.nextLine(); // limpia el buffer
+			sc.nextLine(); // Limpia el buffer
 
 			System.out.println("Introduce el nombre:");
 			String nombre = sc.nextLine();
@@ -134,4 +233,130 @@ public class Principal {
 
 	}
 
+	public static void menuAdmin(Sesion sesion) {
+		String opcionMenu = "";
+		
+		System.out.println(
+				"Hola " + Perfiles.ADMIN + ", bienvenido.");
+		
+		do {
+			System.out.println("-----SELECCIONA UNA OPCION-----");
+			System.out.println("1-Ver espectáculos");
+			System.out.println("2-Gestionar espectáculos");
+			System.out.println("3-Gestionar personas");
+			System.out.println("4-Cerrar sesión");
+			opcionMenu = sc.nextLine();
+
+			switch (opcionMenu) {
+			case "1":
+				mostrarEspectaculos();
+				break;
+			case "2":
+				System.out.println("Gestion de espectáculos");
+				break;
+			case "3":
+				System.out.println("Gestion del personal");
+				break;
+			case "4":
+				System.out.println("Cerrando sesion");
+				sesion.setCredenciales(null);
+				sesion.setPerfil(Perfiles.INVITADO);
+				break;
+
+			default:
+				System.out.println("Opción no valida, vuelve a intentarlo");
+				break;
+			}
+		} while (!opcionMenu.equals("4"));
+
+	}
+
+	public static void menuArtista(Sesion sesion) {
+		String opcionMenu = "";
+		do {
+			System.out.println(
+					"Hola " + Perfiles.ARTISTA + ", selecciona una opción");
+			System.out.println("1-Ver espectáculos");
+			System.out.println("2-Gestionar espectaculos");
+			System.out.println("3-Cerrar sesión");
+			opcionMenu = sc.nextLine();
+
+			switch (opcionMenu) {
+			case "1":
+				mostrarEspectaculos();
+				break;
+			case "2":
+				System.out.println("Gestion de espectáculos");
+				break;
+			case "3":
+				System.out.println("Cerrando sesion");
+				sesion.setCredenciales(null);
+				sesion.setPerfil(Perfiles.INVITADO);
+				break;
+
+			default:
+				System.out.println("Opción no valida, vuelve a intentarlo");
+				break;
+			}
+		} while (!opcionMenu.equals("3"));
+
+	}
+
+	public static void menuCoordinacion(Sesion sesion) {
+		String opcionMenu = "";
+		do {
+			System.out.println(
+					"Hola " + Perfiles.COORDINACION + ", selecciona una opción");
+			System.out.println("1-Ver espectáculos");
+			System.out.println("2-Cerrar sesión");
+			opcionMenu = sc.nextLine();
+
+			switch (opcionMenu) {
+			case "1":
+				mostrarEspectaculos();
+				break;
+			case "2":
+				System.out.println("Cerrando sesion");
+				sesion.setCredenciales(null);
+				sesion.setPerfil(Perfiles.INVITADO);
+				break;
+
+			default:
+				System.out.println("Opción no valida, vuelve a intentarlo");
+				break;
+			}
+		} while (!opcionMenu.equals("2"));
+
+	}
+
+	public static void menuInvitado(Sesion sesion) {
+		String opcionMenu = "";
+
+		do {
+			System.out.println(
+					"Hola " + Perfiles.INVITADO + ", selecciona una opción");
+			System.out.println("1-Ver espectáculos");
+			System.out.println("2-Log in");
+			System.out.println("3-Salir del programa");
+			opcionMenu = sc.nextLine();
+
+			switch (opcionMenu) {
+			case "1":
+				mostrarEspectaculos();
+				break;
+			case "2":
+				logIn(sesion);
+				break;
+			case "3":
+				System.out.println("Saliendo del programa");
+				sesion.setPerfil(null);
+				break;
+
+			default:
+				System.out.println("Opción no valida, vuelve a intentarlo");
+				break;
+			}
+		} while (sesion.getPerfil()==(Perfiles.INVITADO));
+
+	}
 }
